178871fdf1f7c9346face88ba8b9b7d958b2e2d9
GwonYeong <rnjsdud980@naver.com>
2024-12-15 19:48:21 +0900

    Fix: 내가 본 엔딩 css 변경

--- src/app/(main)/main/my/ended-game/_components/SortGame/Card.tsx
+++ src/app/(main)/main/my/ended-game/_components/SortGame/Card.tsx
@@ -30,9 +30,10 @@ export default function Card({
   clickCard,
 }: CardProps) {
   return (
-    <div className="flex flex-col w-[171px] h-[266px]">
+    <div className="flex flex-col justify-between w-[171px] h-[270px]">
+      {/* 이미지 */}
       <div
-        className="w-full h-full aspect-square bg-gray-900 rounded-[8px] overflow-hidden relative"
+        className="w-full  aspect-square bg-gray-900 rounded-[8px] overflow-hidden relative"
         onClick={() => clickCard(cards.id)}
       >
         <Image
@@ -43,45 +44,43 @@ export default function Card({
           height={100}
         />
       </div>
-      <span className="text-white headline-md mt-[8px]">{cards.title}</span>
-      <span className="caption-rg text-gray-200">
-        {GenresKorean[cards.category as keyof typeof GenresKorean]}
-      </span>
-      <div className="flex flex-row ">
-        <div className="flex flex-row w-[2rem] -space-x-2">
-          {users.reduce((prev, curr, idx) => {
-            if (idx > 2) {
-              return prev;
-            }
-            return [...prev, <UserImage key={curr.id} src={curr.imageUrl} />];
-          }, [] as React.ReactNode[])}
+      {/* 장르 */}
+      <div className="flex flex-col h-[90px]  justify-between ">
+        <div className="flex flex-col ">
+          <span className="caption-rg text-gray-200">
+            {GenresKorean[cards.category as keyof typeof GenresKorean]}
+          </span>
+          <span className="text-white headline-md line-clamp-2">
+            {cards.title} 짱짱길면어떻게하지
+          </span>
         </div>
-      </div>
-      <div
-        className="flex flex-row w-[68px] h-[21px]
+
+        <div
+          className="flex flex-row w-[68px] h-[21px]
       bg-gray-800 rounded-[4px]
         justify-center
         gap-[2px]
         mt-[8px]
       "
-      >
-        <div className="flex items-center justify-center">
-          <Svg
-            icon="taleGradientIcon"
-            options={{
-              size: { width: 11, height: 11 },
-              viewBox: "0 0 11 11",
-            }}
-          />
-        </div>
-        <div
-          className="flex  h-[20px] rounded-[8px]
+        >
+          <div className="flex items-center justify-center">
+            <Svg
+              icon="taleGradientIcon"
+              options={{
+                size: { width: 11, height: 11 },
+                viewBox: "0 0 11 11",
+              }}
+            />
+          </div>
+          <div
+            className="flex  h-[20px] rounded-[8px]
            caption-md
             text-transparent bg-clip-text ending-gradiant
             items-center justify-center
           "
-        >
-          엔딩 2/4
+          >
+            엔딩 2/4
+          </div>
         </div>
       </div>
     </div>
