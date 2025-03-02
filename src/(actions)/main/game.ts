11c3d1b4cb348184708be99567f405fee87d3f88
GwonYeong <rnjsdud980@naver.com>
2025-03-02 14:58:44 +0900

    feat: 메인 페이지 페이지네이션 구현

--- src/(actions)/main/game.ts
+++ src/(actions)/main/game.ts
@@ -2,11 +2,14 @@
 
 import { fetchIncetance } from "@/(actions)/fetch";
 import { GenresKorean } from "@/common/Game/Genre";
+import { GAME_LIST_LIMIT } from "@/lib/config";
+import { getCount } from "@choosetale/nestia-type/lib/functional/game_play/list/count";
 import { getList } from "@choosetale/nestia-type/lib/functional/game_play/list/index";
 
 interface GetGameListCallProps {
   genres: (keyof typeof GenresKorean)[];
   order: "LATEST" | "OLDEST" | "POPULAR";
+  page: number;
 }
 
 interface GetStaticGameListCallProps {
@@ -17,13 +20,31 @@ interface GetStaticGameListCallProps {
   };
 }
 
+export const getGameListCountCall = async (
+  genres: (keyof typeof GenresKorean)[]
+): Promise<getCount.Output> => {
+  const queryParams = new URLSearchParams({
+    genre: genres.length > 0 ? genres.join(",") : "ALL",
+  });
+  const res = await fetchIncetance(
+    `${process.env.NEXT_PUBLIC_BACKEND_API}${
+      getCount.METADATA.path
+    }?${queryParams.toString()}`,
+    {
+      method: getCount.METADATA.method,
+    }
+  );
+  return await res.json();
+};
+
 export const getGameListCall = async ({
   genres,
   order,
+  page,
 }: GetGameListCallProps): Promise<getList.Output> => {
   const queryParams = new URLSearchParams({
-    page: "1",
-    limit: "10",
+    page: page.toString(),
+    limit: GAME_LIST_LIMIT.toString(),
     order,
     genre: genres.length > 0 ? genres.join(",") : "ALL",
   });
--- src/app/(main)/main/game/(client)/GameLIst.tsx
+++ src/app/(main)/main/game/(client)/GameLIst.tsx
@@ -1,35 +1,79 @@
 "use client";
 
-import { getGameListCall } from "@/(actions)/main/game";
-import React, { useEffect, useState } from "react";
+import { getGameListCall, getGameListCountCall } from "@/(actions)/main/game";
+import React, { useCallback, useEffect, useState } from "react";
 import CardList from "../_components/CardList";
 import { GameListEntity } from "../../../_domain/game-list.entity";
 import { useGameFilterStore } from "@/store/Game/GameFilter.store";
 import { useGameOrderStore } from "@/store/Game/GameOrder.store";
-
+import { useInView } from "react-intersection-observer";
 export default function GameList() {
+  const [page, setPage] = useState(1);
   const [gameList, setGameList] = useState<GameListEntity>(
     new GameListEntity([])
   );
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [ref, inView] = useInView();
 
   const selectedGenres = useGameFilterStore((state) => state.selectedGenres);
   const selectedOrder = useGameOrderStore((state) => state.selectedOrder);
 
+  const [totalCount, setTotalCount] = useState(0);
+
   useEffect(() => {
+    const fetchTotalCount = async () => {
+      const count = await getGameListCountCall(selectedGenres);
+
+      setTotalCount(count.count);
+    };
+
+    fetchTotalCount();
+  }, [selectedGenres]);
+
+  useEffect(() => {
+    if (
+      inView &&
+      !isLoading &&
+      gameList.gameList.length > 0 &&
+      gameList.gameList.length < totalCount
+    ) {
+      setPage((page) => page + 1);
+    }
+  }, [inView, isLoading, gameList.gameList.length, totalCount]);
+
+  useEffect(() => {
+    setGameList(new GameListEntity([]));
+    setPage(1);
+  }, [selectedGenres, selectedOrder]);
+
+  useEffect(() => {
+    if (page === 0) return;
+
     const getGameList = async () => {
-      const res = await getGameListCall({
-        genres: selectedGenres,
-        order: selectedOrder,
-      });
-      setGameList(new GameListEntity(res));
+      setIsLoading(true);
+      try {
+        const res = await getGameListCall({
+          genres: selectedGenres,
+          order: selectedOrder,
+          page,
+        });
+        console.log(res);
+        setGameList((prev) => new GameListEntity([...prev.gameList, ...res]));
+      } catch (error) {
+        console.error("Failed to fetch game list:", error);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     getGameList();
-  }, [selectedGenres, selectedOrder]);
+  }, [selectedGenres, selectedOrder, page]);
 
   return (
     <div className="flex flex-col h-full">
       <CardList gameList={gameList} />
+      {!isLoading && <div ref={ref} className="h-[1px]" />}
     </div>
   );
 }
