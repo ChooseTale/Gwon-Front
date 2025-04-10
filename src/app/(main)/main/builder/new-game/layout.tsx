7cd8b6f2f4e9bbcd72913090e2ae06374372e78a
GwonYeong <rnjsdud980@naver.com>
2025-04-10 20:50:37 +0900

    fix: 빌드 가능 처리

--- src/app/(main)/main/builder/new-game/layout.tsx
+++ src/app/(main)/main/builder/new-game/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import MarginWrapper from "../../_component/MarginWrapper";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <div className="flex flex-col w-full">{children}</div>;
